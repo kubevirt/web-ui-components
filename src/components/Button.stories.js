74a2340191d182ddb5e24e69b473a596b7854fbe
Vojtech Szocs <vojtech.szocs@gmail.com>
2018-08-24 14:29:37 +0200

    Add initial project structure based on create-react-app (#1)
    * Initial project structure based on create-react-app template
    * Add initial Storybook config and patternfly-react dependency
    * Rename create-react-app generated README
    
    Signed-off-by: Vojtech Szocs <vszocs@redhat.com>

---
+++ src/components/Button.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Button } from 'patternfly-react';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with another text', () => (
+    <Button onClick={action('clicked')}>Foo Bar</Button>
+  ));   
