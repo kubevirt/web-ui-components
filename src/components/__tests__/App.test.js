f7a88443e3fdaaa836b3df4592c3e4ed2ff03076
Rastislav Wagner <rawagner@redhat.com>
2018-09-04 07:48:29 +0200

    Add Enzyme deps and configuration

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import { shallow } from 'enzyme';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('shallow render matches snapshot', () => {
+    const app = shallow(<App />);
+    expect(app).toMatchSnapshot();
+  });
+});
