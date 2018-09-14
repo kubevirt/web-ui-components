159023993b27d6e48d0b88c5998df1b2b00da669
Marek Libra <mlibra@redhat.com>
2018-09-14 13:12:24 +0200

    Adapt project to be an npm package
    The project will now on hold just reusable components
    published as npm packages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export * from './components';
