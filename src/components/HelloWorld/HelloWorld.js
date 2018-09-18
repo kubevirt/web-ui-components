89125b8cbd6050c5f9ab93485fe6a3777604ca7a
Marek Libra <mlibra@redhat.com>
2018-09-18 13:09:29 +0200

    Babelize "dist" result
    By distributing babelized output instead of plain component sources,
    we can use more JavaScript features of our choice.

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const HelloWorld = () => <div>Hello World!</div>;
+class HelloWorld extends React.Component {
+  state = { world: this.props.world };
+
+  render() {
+    return <div className="hello-world">Hello {this.state.world}!</div>;
+  }
+}
+
+HelloWorld.propTypes = {
+  world: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+  world: 'World'
+};
 
 export default HelloWorld;
