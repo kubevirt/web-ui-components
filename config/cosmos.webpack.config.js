39aa36b0274d87c78d52c0f3d7d4e4ac82d4ab39
Vojtech Szocs <vojtech.szocs@gmail.com>
2018-09-27 18:52:04 +0200

    Apply Sass stylesheets to Cosmos webpack config (#30)

--- config/cosmos.webpack.config.js
+++ config/cosmos.webpack.config.js
@@ -7,6 +7,16 @@ const babelOptions = require('./babel.config');
 const webpackMode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 console.log(chalk`Running webpack in {white ${webpackMode}} mode.`);
 
+// https://github.com/sass/node-sass#options
+const sassOptions = {
+  includePaths: [
+    `${paths.nodeModules}/bootstrap-sass/assets/stylesheets`,
+    `${paths.nodeModules}/font-awesome-sass/assets/stylesheets`,
+    `${paths.nodeModules}/patternfly/dist/sass`,
+    `${paths.nodeModules}/patternfly-react/dist/sass`
+  ]
+};
+
 module.exports = {
   mode: webpackMode,
 
@@ -19,12 +29,19 @@ module.exports = {
         options: babelOptions
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: sassOptions
+          }
+        ]
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|gif|jpg|png)$/,
-        loader: 'url-loader'
+        test: /\.(jpg|jpeg|png|gif|svg|eot|otf|ttf|woff|woff2)$/,
+        loader: 'file-loader'
       }
     ]
   }
--- config/paths.js
+++ config/paths.js
@@ -5,8 +5,10 @@ const pathTo = relativePath => path.join(projectRoot, relativePath);
 module.exports = {
   projectRoot,
   packageJson: pathTo('package.json'),
+  nodeModules: pathTo('node_modules'),
   src: pathTo('src'),
   components: pathTo('src/components'),
+  sass: pathTo('sass'),
   config: pathTo('config'),
   tools: pathTo('tools'),
   coverage: pathTo('coverage'),
