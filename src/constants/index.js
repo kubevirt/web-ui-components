8b4c76b396aeeb38d89fd79bc2e36722f0036754
Rastislav Wagner <rawagner@redhat.com>
2018-10-24 11:08:35 +0200

    Make constants out of pxe annotations and other strings found in k8s/request.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,32 +7,42 @@ import { linuxUserTemplate } from '../k8s/mock_user_templates/linux.template';
 import { windowsUserTemplate } from '../k8s/mock_user_templates/windows.template';
 import { network1 } from '../k8s/mock_network/network.template';
 
-export const API_VERSION = 'kubevirt.io/v1alpha2';
-export const OS_LABEL = 'kubevirt.io/os';
-export const FLAVOR_LABEL = 'kubevirt.io/flavor';
-export const CLOUDINIT_VOLUME = 'cloudinitvolume';
-export const CLOUDINIT_DISK = 'cloudinitdisk';
-export const REGISTRY_VOLUME = 'registryvolume';
-export const REGISTRY_DISK = 'registrydisk';
-export const VIRTIO_BUS = 'virtio';
-export const TEMPLATE_OS_LABEL = 'os.template.cnv.io';
-export const TEMPLATE_FLAVOR_LABEL = 'flavor.template.cnv.io';
-export const TEMPLATE_WORKLOAD_LABEL = 'workload.template.cnv.io';
-export const TEMPLATE_TYPE_LABEL = 'template.cnv.io/type';
-export const TEMPLATE_TYPE_VM = 'vm';
-export const TEMPLATE_TYPE_BASE = 'base';
-export const CUSTOM_FLAVOR = 'Custom';
 export const ANNOTATION_DEFAULT_DISK = 'defaults.template.cnv.io/disk';
 export const ANNOTATION_DEFAULT_NETWORK = 'defaults.template.cnv.io/network';
-export const PARAM_VM_NAME = 'NAME';
-export const PROVISION_SOURCE_URL = 'URL';
+export const ANNOTATION_FIRST_BOOT = 'cnv.ui.firstBoot';
+export const ANNOTATION_PXE_INTERFACE = 'cnv.ui.pxeInterface';
+
+export const CLOUDINIT_DISK = 'cloudinitdisk';
+export const CLOUDINIT_NOCLOUD = 'cloudInitNoCloud';
+export const CLOUDINIT_VOLUME = 'cloudinitvolume';
+export const CUSTOM_FLAVOR = 'Custom';
+export const FLAVOR_LABEL = 'kubevirt.io/flavor';
+export const KUBEVIRT_API_VERSION = 'kubevirt.io/v1alpha2';
+export const OS_LABEL = 'kubevirt.io/os';
+export const POD_NETWORK = 'Pod Networking';
+
 export const PROVISION_SOURCE_PXE = 'PXE';
 export const PROVISION_SOURCE_REGISTRY = 'Registry';
 export const PROVISION_SOURCE_TEMPLATE = 'Template';
+export const PROVISION_SOURCE_URL = 'URL';
 
-export const POD_NETWORK = 'Pod Networking';
+export const PVC_ACCESSMODE_RWO = 'ReadWriteOnce';
+
+export const REGISTRY_DISK = 'registrydisk';
+export const REGISTRY_VOLUME = 'registryvolume';
+
+export const TEMPLATE_API_VERSION = 'template.openshift.io/v1';
+export const TEMPLATE_FLAVOR_LABEL = 'flavor.template.cnv.io';
+export const TEMPLATE_OS_LABEL = 'os.template.cnv.io';
+export const TEMPLATE_PARAM_VM_NAME = 'NAME';
+export const TEMPLATE_TYPE_LABEL = 'template.cnv.io/type';
+export const TEMPLATE_TYPE_VM = 'vm';
+export const TEMPLATE_TYPE_BASE = 'base';
+export const TEMPLATE_WORKLOAD_LABEL = 'workload.template.cnv.io';
+
+export const VIRTIO_BUS = 'virtio';
 
 export const baseTemplates = [fedora28, ubuntu1804, rhel75, rhelHighPerformance, windows];
+export const networkConfigs = [network1];
 export const userTemplates = [linuxUserTemplate, windowsUserTemplate];
 export const templates = [...baseTemplates, ...userTemplates];
-export const networkConfigs = [network1];
