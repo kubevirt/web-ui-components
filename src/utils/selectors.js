c39a32c0853df47fa8ffa320d1bc4500a47a6cf8
rawagner <rawagner@redhat.com>
2018-11-07 13:52:49 +0100

    import-vm-apb template is using datavolumes not pvc (#88)

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -3,13 +3,18 @@ import { get } from 'lodash';
 export const getName = value => get(value, 'metadata.name');
 export const getNamespace = value => get(value, 'metadata.namespace');
 
-export const getGibStorageSize = (units, object) => {
-  const size = get(object, 'spec.resources.requests.storage');
+export const getGibStorageSize = (units, resources) => {
+  const size = get(resources, 'requests.storage');
   return size ? units.dehumanize(size, 'binaryBytesWithoutB').value / 1073741824 : null; // 1024^3
 };
 
-export const getStorageClassName = object => get(object, 'spec.storageClassName');
+export const getPvcResources = pvc => get(pvc, 'spec.resources');
+export const getDataVolumeResources = dataVolume => get(dataVolume, 'spec.pvc.resources');
+
+export const getPvcStorageClassName = pvc => get(pvc, 'spec.storageClassName');
+
+export const getDataVolumeStorageClassName = dataVolume => get(dataVolume, 'spec.pvc.storageClassName');
 
 export const getDisks = vm => get(vm, 'spec.template.spec.domain.devices.disks', []);
 export const getVolumes = vm => get(vm, 'spec.template.spec.volumes', []);
-export const getPersistentVolumeClaimName = volume => get(volume, 'persistentVolumeClaim.claimName');
+export const getDataVolumes = vm => get(vm, 'spec.dataVolumeTemplates', []);
