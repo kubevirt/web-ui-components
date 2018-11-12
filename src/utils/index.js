08629baa39d7dfd309c08769d0dd3cfb1f53ec59
Rastislav Wagner <rawagner@redhat.com>
2018-11-12 10:39:46 +0100

    Add PXE boot patch util method

---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+export * from './selectors';
+export * from './templates';
+export * from './utils';
+export * from './validations';
--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -16,5 +16,6 @@ export const getPvcStorageClassName = pvc => get(pvc, 'spec.storageClassName');
 export const getDataVolumeStorageClassName = dataVolume => get(dataVolume, 'spec.pvc.storageClassName');
 
 export const getDisks = vm => get(vm, 'spec.template.spec.domain.devices.disks', []);
+export const getInterfaces = vm => get(vm, 'spec.template.spec.domain.devices.interfaces', []);
 export const getVolumes = vm => get(vm, 'spec.template.spec.volumes', []);
 export const getDataVolumes = vm => get(vm, 'spec.dataVolumeTemplates', []);
