019871cb8290812e72fd69fb4d37d69fd3cb0e12
suomiy <suomiy@gmail.com>
2019-01-15 14:35:15 +0100

    add url and container validations (#181)

--- src/components/Wizard/CreateVmWizard/BasicSettingsTab.js
+++ src/components/Wizard/CreateVmWizard/BasicSettingsTab.js
@@ -5,7 +5,7 @@ import { get } from 'lodash';
 import { FormFactory, validateForm } from '../../Form/FormFactory';
 import { getName, getMemory, getCpu, getCloudInitUserData } from '../../../utils/selectors';
 import { getTemplate, getTemplateProvisionSource } from '../../../utils/templates';
-import { validateDNS1123SubdomainValue } from '../../../utils/validations';
+import { validateDNS1123SubdomainValue, validateURL, validateContainer } from '../../../utils/validations';
 import {
   NO_TEMPLATE,
   HELP_PROVISION_SOURCE_URL,
@@ -144,6 +144,7 @@ export const getFormFields = (basicSettings, namespaces, templates, selectedName
       required: true,
       isVisible: basicVmSettings => isImageSourceType(basicVmSettings, PROVISION_SOURCE_CONTAINER),
       disabled: userTemplate !== undefined,
+      validate: validateContainer,
     },
     [IMAGE_URL_KEY]: {
       id: 'provision-source-url',
@@ -151,6 +152,7 @@ export const getFormFields = (basicSettings, namespaces, templates, selectedName
       required: true,
       isVisible: basicVmSettings => isImageSourceType(basicVmSettings, PROVISION_SOURCE_URL),
       disabled: userTemplate !== undefined,
+      validate: validateURL,
     },
     [OPERATING_SYSTEM_KEY]: {
       id: 'operating-system-dropdown',
--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,6 +1,11 @@
+export const EMPTY_ERROR = 'cannot be empty';
+export const START_WHITESPACE_ERROR = 'cannot start with whitespace characters';
+export const END_WHITESPACE_ERROR = 'cannot end with whitespace characters';
+
 export const DNS1123_START_ERROR = 'has to start with alphanumeric character';
 export const DNS1123_END_ERROR = 'has to end with alphanumeric character';
 export const DNS1123_CONTAINS_ERROR = 'cannot contain';
-export const DNS1123_EMPTY_ERROR = 'cannot be empty';
 export const DNS1123_TOO_LONG_ERROR = 'cannot have more than 253 characters';
 export const DNS1123_UPPERCASE_ERROR = 'cannot contain uppercase letters';
+
+export const URL_INVALID_ERROR = 'has to be a valid URL';
