2d1b52876dc97ceecb34cc2943fe24eaf0189b2a
Marek Libra <marek.libra@gmail.com>
2019-02-19 13:59:46 +0100

    Dropdown: overflow for long values (#226)
    * Dropdown: overflow for long values
    
    * Dropdown: move overflow tweaks under patternfly-tweaks/dropdown.scss

--- src/components/Form/Dropdown.js
+++ src/components/Form/Dropdown.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ButtonGroup, DropdownButton, MenuItem, noop } from 'patternfly-react';
+import { ButtonGroup, DropdownButton, MenuItem, noop, Tooltip, OverlayTrigger } from 'patternfly-react';
 
-export const Dropdown = ({ id, value, disabled, onChange, choices }) => {
+export const Dropdown = ({ id, value, disabled, onChange, choices, withTooltips }) => {
   const title = typeof value === 'object' ? value.name || value.id : value;
   return (
     <ButtonGroup justified key={id}>
@@ -19,11 +19,21 @@ export const Dropdown = ({ id, value, disabled, onChange, choices }) => {
           const key = isObject ? choice.id || choice.name : choice;
           const val = isObject ? choice.name : choice;
 
-          return (
+          const content = (
             <MenuItem key={key} eventKey={choice}>
               {val}
             </MenuItem>
           );
+
+          if (withTooltips) {
+            const tooltip = <Tooltip id={`tooltip-${key}`}>{val}</Tooltip>;
+            return (
+              <OverlayTrigger key={key} overlay={tooltip} placement="top">
+                {content}
+              </OverlayTrigger>
+            );
+          }
+          return content;
         })}
       </DropdownButton>
     </ButtonGroup>
@@ -34,6 +44,7 @@ Dropdown.defaultProps = {
   onChange: noop,
   disabled: false,
   choices: [],
+  withTooltips: false,
 };
 
 Dropdown.propTypes = {
@@ -42,4 +53,5 @@ Dropdown.propTypes = {
   choices: PropTypes.array,
   onChange: PropTypes.func,
   disabled: PropTypes.bool,
+  withTooltips: PropTypes.bool,
 };
