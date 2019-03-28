44e10756c3a583384c6c4efcaa7c8f81aa607c74
Neha <gnehapk@gmail.com>
2019-03-28 18:23:19 +0530

    Fixed inventory issue

--- src/components/ClusterOverview/StorageOverview.js
+++ src/components/ClusterOverview/StorageOverview.js
@@ -5,12 +5,16 @@ import { Grid, GridItem } from '@patternfly/react-core';
 import { Dashboard, DashboardBody, DashboardHeader } from '../Dashboard';
 
 import { StorageDetailsConnected } from './Details/storageDetails'; // TODO: re-export
+import InventoryConnected from './Inventory/Inventory';
 
 const LeftCards = () => (
   <GridItem key="left" lg={3} md={12} sm={12}>
     <Grid>
       <GridItem lg={12} md={6} sm={12}>
         <StorageDetailsConnected />
+      </GridItem>
+      <GridItem lg={12} md={6} sm={12}>
+        <InventoryConnected />
       </GridItem>
     </Grid>
   </GridItem>
