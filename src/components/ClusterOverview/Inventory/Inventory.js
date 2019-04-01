86462d17a96ad3afcfc0ada48c3a78b44a5fc78e
Ankush Behl <cloudbehl@gmail.com>
2019-04-01 22:43:12 +0530

    Removed inventory heading

--- src/components/ClusterOverview/Inventory/Inventory.js
+++ src/components/ClusterOverview/Inventory/Inventory.js
@@ -42,7 +42,7 @@ InventoryBody.propTypes = {
 export const Inventory = props => (
   <DashboardCard>
     <DashboardCardHeader>
-      <DashboardCardTitle>{heading}</DashboardCardTitle>
+      <DashboardCardTitle>Inventory</DashboardCardTitle>
       <DashboardCardTitleHelp>help for inventory</DashboardCardTitleHelp>
     </DashboardCardHeader>
     <DashboardCardBody className="kubevirt-inventory__body">
