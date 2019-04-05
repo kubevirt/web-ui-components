af7008499e8dc7a9f95b723bd81fd0b999fdc5f4
Marek Libra <mlibra@redhat.com>
2019-04-05 13:26:05 +0200

    Dashboard, Utilization: Render in narrow container
    The Utilization component renders different sub-component structure
    for a narrow container.

--- src/components/Dashboard/Utilization/UtilizationItem.js
+++ src/components/Dashboard/Utilization/UtilizationItem.js
@@ -1,60 +1,109 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Measure from 'react-measure';
 
 import { Col, Row, SparklineChart } from 'patternfly-react';
+
+import { MEDIA_QUERY_SM } from '../../../utils';
 
 import { InlineLoading } from '../../Loading';
 import { NOT_AVAILABLE } from './strings';
 
-export const UtilizationItem = ({ id, title, data, maxY, unit, isLoading, LoadingComponent }) => {
-  const axis = {
-    y: {
-      show: true,
-      padding: {
-        top: 0,
-        bottom: 0,
-      },
-      tick: {
-        count: 3,
-      },
-    },
-    x: {
-      show: false,
+export class UtilizationItem extends React.PureComponent {
+  state = {
+    dimensions: {
+      width: -1,
     },
   };
-  if (maxY) {
-    axis.y.max = maxY;
-  }
 
-  let actual;
-  let chartData;
-  if (!isLoading && data) {
-    actual = Math.round(data[data.length - 1]);
-    chartData = {
-      columns: [[unit, ...data.map(val => val.toFixed(1))]],
-      unload: true,
-      type: 'area',
+  onResize = contentRect => {
+    this.setState({ dimensions: contentRect.bounds });
+  };
+
+  render() {
+    const { id, title, data, maxY, unit, isLoading, LoadingComponent } = this.props;
+    const { width } = this.state.dimensions;
+
+    const axis = {
+      y: {
+        show: true,
+        padding: {
+          top: 0,
+          bottom: 0,
+        },
+        tick: {
+          count: 3,
+        },
+      },
+      x: {
+        show: false,
+      },
     };
-  }
+    if (maxY) {
+      axis.y.max = maxY;
+    }
 
-  return (
-    <div className="kubevirt-utilization__item">
-      <Row>
-        <Col lg={2} md={2} sm={2} xs={2}>
-          {title}
-        </Col>
-        <Col className="kubevirt-utilization__item-actual" lg={3} md={3} sm={3} xs={3}>
-          {isLoading || !data || `${actual} ${unit}`}
-        </Col>
-        <Col className="kubevirt-utilization__item-chart" lg={7} md={7} sm={7} xs={7}>
-          {isLoading && <LoadingComponent />}
-          {!isLoading && data && <SparklineChart id={id} data={chartData} axis={axis} unloadBeforeLoad />}
-          {!isLoading && !data && NOT_AVAILABLE}
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    let actual;
+    let chart = NOT_AVAILABLE;
+    if (isLoading) {
+      chart = <LoadingComponent />;
+    } else if (data) {
+      const chartData = {
+        columns: [[unit, ...data.map(val => val.toFixed(1))]],
+        unload: true,
+        type: 'area',
+      };
+      actual = `${Math.round(data[data.length - 1])} ${unit}`;
+      chart = <SparklineChart id={id} data={chartData} axis={axis} unloadBeforeLoad />;
+    }
+
+    let topClass;
+    let rows;
+    if (width < MEDIA_QUERY_SM) {
+      topClass = 'kubevirt-utilization__item-narrow';
+      rows = (
+        <Fragment>
+          <Row>
+            <Col lg={6} md={6} sm={6} xs={6}>
+              {title}
+            </Col>
+            <Col className="kubevirt-utilization__item-actual" lg={6} md={6} sm={6} xs={6}>
+              {actual}
+            </Col>
+          </Row>
+          <Row>
+            <Col className="kubevirt-utilization__item-narrow-chart">{chart}</Col>
+          </Row>
+        </Fragment>
+      );
+    } else {
+      topClass = 'kubevirt-utilization__item';
+      rows = (
+        <Row>
+          <Col lg={2} md={2} sm={2} xs={2}>
+            {title}
+          </Col>
+          <Col className="kubevirt-utilization__item-actual" lg={3} md={3} sm={3} xs={3}>
+            {actual}
+          </Col>
+          <Col className="kubevirt-utilization__item-chart" lg={7} md={7} sm={7} xs={7}>
+            {chart}
+          </Col>
+        </Row>
+      );
+    }
+
+    return (
+      <Measure bounds onResize={this.onResize}>
+        {({ measureRef }) => (
+          <div ref={measureRef} className={topClass}>
+            {rows}
+          </div>
+        )}
+      </Measure>
+    );
+  }
+}
 
 UtilizationItem.defaultProps = {
   maxY: null,
