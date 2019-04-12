ba8c5d53b9226ea58c14465142af9bfc97254c99
Jiri Tomasek <jtomasek@redhat.com>
2019-04-12 11:12:57 +0200

    Add default host status when host does not have status data

--- src/utils/status/host/hostStatus.js
+++ src/utils/status/host/hostStatus.js
@@ -1,17 +1,17 @@
-import { getOperationalStatus, getProvisioningState } from '../../../selectors';
+import { getOperationalStatus, getProvisioningState, getHostErrorMessage } from '../../../selectors';
 
-import { HOST_STATUS_TO_TEXT, HOST_STATUS_READY } from './constants';
+import { HOST_STATUS_TO_TEXT, HOST_STATUS_READY, HOST_STATUS_REGISTERING } from './constants';
 
 export const getHostStatus = host => {
   // Returns a status string based on the available host information.
   const operationalStatus = getOperationalStatus(host);
   const provisioningState = getProvisioningState(host);
 
-  const hostStatus = provisioningState || operationalStatus;
+  const hostStatus = provisioningState || operationalStatus || HOST_STATUS_REGISTERING;
   return {
     status: hostStatus,
     text: HOST_STATUS_TO_TEXT[hostStatus] || hostStatus,
-    errorMessage: host.status.errorMessage,
+    errorMessage: getHostErrorMessage(host),
   };
 };
 
