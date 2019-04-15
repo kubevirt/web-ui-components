d371b15b6756959ea8f75d0937c2ca5ca4c24f25
Jiri Tomasek <jtomasek@redhat.com>
2019-04-15 15:55:56 +0200

    Remove unused fixture.expected call from tests

--- src/utils/status/host/tests/hostStatus.test.js
+++ src/utils/status/host/tests/hostStatus.test.js
@@ -3,7 +3,7 @@ import hostFixtures from '../fixtures/hostStatus.fixture';
 
 describe('getHostStatus()', () => {
   hostFixtures.forEach((fixture, idx) => {
-    const resultStatus = fixture.expected || fixture.expectedSimple;
+    const resultStatus = fixture.expectedSimple;
     it(`return status correctly ${idx} (${resultStatus})`, () => {
       expect(getHostStatus(fixture.host).status).toBe(resultStatus);
     });
@@ -12,7 +12,7 @@ describe('getHostStatus()', () => {
 
 describe('getSimpleHostStatus()', () => {
   hostFixtures.forEach((fixture, idx) => {
-    const resultStatus = fixture.expected || fixture.expectedSimple;
+    const resultStatus = fixture.expectedSimple;
     it(`return status correctly ${idx} (${resultStatus})`, () => {
       expect(getSimpleHostStatus(fixture.host)).toBe(resultStatus);
     });
