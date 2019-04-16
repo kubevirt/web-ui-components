618a095a3a47dda95a5a6f7359013b7fe87a24d7
rawagner <rawagner@redhat.com>
2019-04-16 11:32:09 +0200

    Use PF4 graphs (#351)

--- src/components/Dashboard/Capacity/CapacityItem.js
+++ src/components/Dashboard/Capacity/CapacityItem.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { DonutChart } from 'patternfly-react';
+import { ChartDonut, ChartTheme, ChartLabel, ChartTooltip } from '@patternfly/react-charts';
 import PropTypes from 'prop-types';
 
 import { InlineLoading } from '../../Loading';
 import { prefixedId } from '../../../utils';
 import { USED, AVAILABLE, LOADING, CAPACITY, UNKNOWN, NOT_AVAILABLE } from './strings';
 
-const pfGetUtilizationDonutTooltipContents = d =>
-  `<span class="donut-tooltip-pf" style="white-space: nowrap;">${Math.round(d[0].ratio * 1000) / 10} ${
-    d[0].name
-  }</span>`;
+const CHART_WIDTH = 200;
+const CHART_HEIGHT = 200;
+
+const TITLE_HEIGHT_OFFSET = 12;
 
 export const CapacityItem = ({ id, title, used, total, unit, formatValue, LoadingComponent, isLoading }) => {
   let description;
-  let columns = [[USED, 0], [AVAILABLE, 100]];
-  let tooltip;
+  let data = [{ x: USED, y: 0 }, { x: AVAILABLE, y: 100 }];
   let primaryTitle;
   let secondaryTitle = CAPACITY;
   if (isLoading) {
@@ -27,43 +26,63 @@ export const CapacityItem = ({ id, title, used, total, unit, formatValue, Loadin
     const totalFormatted = formatValue(total, unit);
     const usedFormatted = formatValue(used, totalFormatted.unit);
 
-    unit = totalFormatted.unit; // eslint-disable-line prefer-destructuring
-    total = totalFormatted.value;
-    used = usedFormatted.value;
+    const formattedUnit = totalFormatted.unit; // eslint-disable-line prefer-destructuring
+    const formattedTotal = totalFormatted.value;
+    const formattedUsed = usedFormatted.value;
 
-    const available = total - used;
-    columns = [[USED, used], [AVAILABLE, available]];
+    const available = formattedTotal - formattedUsed;
 
-    const percentageUsed = total > 0 ? Math.round((100 * used) / total) : 0;
+    const percentageUsed = total > 0 ? Math.round((100 * formattedUsed) / formattedTotal) : 0;
     const availableNice = Number.isInteger(available) ? available : available.toFixed(1);
-    const totalNice = Number.isInteger(total) ? total : total.toFixed(1);
+    const totalNice = Number.isInteger(formattedTotal) ? formattedTotal : formattedTotal.toFixed(1);
+    const usedNice = Number.isInteger(formattedUsed) ? formattedUsed : formattedUsed.toFixed(1);
 
-    description = `${availableNice} ${unit} available out of ${totalNice} ${unit}`;
-    tooltip = { contents: pfGetUtilizationDonutTooltipContents };
+    data = [
+      {
+        x: USED,
+        y: formattedUsed,
+        label: `${USED}: ${usedNice}${formattedUnit}`,
+      },
+      {
+        x: AVAILABLE,
+        y: available,
+        label: `${AVAILABLE}: ${availableNice}${formattedUnit}`,
+      },
+    ];
+    description = `${availableNice} ${formattedUnit} available out of ${totalNice} ${formattedUnit}`;
     primaryTitle = `${percentageUsed.toString()}%`;
     secondaryTitle = USED;
   }
-  // TODO(mlibra): align text font
   return (
     <div className="kubevirt-capacity__item">
       <h3 className="kubevirt-capacity__item-title">{title}</h3>
       <h6>{description}</h6>
-      <div>
-        <DonutChart
-          id={prefixedId('donut-chart', id)}
-          size={{ width: 150, height: 150 }}
-          data={{
-            unload: true,
-            columns,
-            order: null,
-          }}
-          unloadBeforeLoad
-          tooltip={tooltip}
-          title={{
-            primary: primaryTitle,
-            secondary: secondaryTitle,
-          }}
-        />
+      <div id={prefixedId('donut-chart', id)} className="kubevirt-capacity__graph">
+        <svg viewBox={`0 0 ${CHART_WIDTH} ${CHART_HEIGHT}`}>
+          <ChartDonut
+            data={data}
+            labels={datum => datum.label}
+            theme={ChartTheme.light.blue}
+            height={CHART_HEIGHT}
+            width={CHART_WIDTH}
+            standalone={false}
+            labelComponent={<ChartTooltip style={{ fontSize: 20 }} />}
+          />
+          <ChartLabel
+            textAnchor="middle"
+            style={{ fontSize: 25 }}
+            x={CHART_WIDTH / 2}
+            y={CHART_HEIGHT / 2 - TITLE_HEIGHT_OFFSET}
+            text={primaryTitle}
+          />
+          <ChartLabel
+            textAnchor="middle"
+            style={{ fontSize: 15 }}
+            x={CHART_WIDTH / 2}
+            y={CHART_HEIGHT / 2 + TITLE_HEIGHT_OFFSET}
+            text={secondaryTitle}
+          />
+        </svg>
       </div>
     </div>
   );
