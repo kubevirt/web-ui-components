bde13594fc44f1418c98169ab2a147819f6c5667
yaacov <kobi.zamir@gmail.com>
2019-04-16 14:32:36 +0300

    move status subtitle down

--- src/components/Dashboard/Health/HealthItem.js
+++ src/components/Dashboard/Health/HealthItem.js
@@ -6,8 +6,8 @@ export const HealthItem = ({ icon, classname, message, details, isRow }) => (
   <div className={isRow ? 'kubevirt-health__body kubevirt-health__row' : 'kubevirt-health__body'}>
     <Icon type="fa" size="2x" name={icon} className={`kubevirt-health__icon kubevirt-health__icon--${classname}`} />
     <div className="kubevirt-health__message">
-      <span className="kubevirt-health__title">{message}</span>
-      <span className="kubevirt-health__title kubevirt-health__subtitle">{details}</span>
+      <div className="kubevirt-health__title">{message}</div>
+      <div className="kubevirt-health__title kubevirt-health__subtitle">{details}</div>
     </div>
   </div>
 );
