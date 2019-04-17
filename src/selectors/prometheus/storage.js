0fb92e4b87c7f4d9c7f8f35c88f689d90e0bb5fb
Neha <gnehapk@gmail.com>
2019-04-17 17:47:13 +0900

    Modified top consumer card as per the pf4 charts

--- src/selectors/prometheus/storage.js
+++ src/selectors/prometheus/storage.js
@@ -1,25 +1,17 @@
 import { flatMap, max } from 'lodash';
 
-import { parseNumber, formatBytes } from '../../utils';
+import { parseNumber, formatBytes, formatToShortTime } from '../../utils';
 
 export const getTopConsumerVectorStats = result => {
   let maxVal = 0;
 
   const namespaceValues = flatMap(result, namespace => namespace.values);
   const allBytes = flatMap(namespaceValues, value => parseNumber(value[1]));
-
   maxVal = max(allBytes);
-
   const maxCapacityConverted = { ...formatBytes(maxVal) };
-
-  const yAxisData = result.map(r => [
-    r.metric.namespace,
-    ...r.values.map(array => formatBytes(array[1], maxCapacityConverted.unit, 2).value),
-  ]);
 
   let largestLengthArray = 0;
   let largestLengthArrayIndex = 0;
-
   // timestamps count and values are not same for all the namespaces. Hence, finding the largest length array and taking its timestamps value as x-axis point
   result.forEach((namespace, index) => {
     const len = namespace.values.length;
@@ -28,10 +20,39 @@ export const getTopConsumerVectorStats = result => {
       largestLengthArrayIndex = index;
     }
   });
-  const xAxisData = ['x', ...result[largestLengthArrayIndex].values.map(array => new Date(array[0] * 1000))];
+  const xAxisData = [
+    ...result[largestLengthArrayIndex].values.map(array => formatToShortTime(new Date(array[0] * 1000))),
+  ];
 
+  const sortNamespaces = (a, b) => {
+    let isALarger = 0;
+    a = a.metric.namespace.toLowerCase();
+    b = b.metric.namespace.toLowerCase();
+    if (a > b) {
+      isALarger = 1;
+    } else if (a < b) {
+      isALarger = -1;
+    }
+
+    return isALarger;
+  };
+  const sortedResult = result.sort(sortNamespaces);
+
+  const legends = sortedResult.map(r => ({ name: r.metric.namespace }));
+
+  const chartData = sortedResult.map(r =>
+    r.values.map(arr => [
+      formatToShortTime(new Date(arr[0] * 1000)),
+      formatBytes(arr[1], maxCapacityConverted.unit, 2).value,
+    ])
+  );
+
+  // sorting namespaces to maintain the order of legends displayed for chart
   const stats = {
-    columns: [xAxisData, ...yAxisData],
+    chartData,
+    legends,
+    xAxisData,
+    maxCapacity: Number(maxCapacityConverted.value),
     unit: maxCapacityConverted.unit,
   };
 
