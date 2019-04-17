16bb6dfdd8a43061f52c9990f019cdcb452a4533
Neha <gnehapk@gmail.com>
2019-04-17 17:47:13 +0900

    Modified top consumer card as per the pf4 charts

--- src/components/StorageOverview/TopConsumers/tests/TopConsumers.test.js
+++ src/components/StorageOverview/TopConsumers/tests/TopConsumers.test.js
@@ -4,17 +4,28 @@ import { render } from 'enzyme';
 import { TopConsumers } from '../TopConsumers';
 import { TopConsumerStats } from '../fixtures/TopConsumers.fixture';
 
+/* eslint no-global-assign: 0 */
 describe('<TopConsumers />', () => {
   it('no-data-renders-correctly', () => {
     const component = render(<TopConsumers {...TopConsumerStats[0]} />);
     expect(component).toMatchSnapshot();
   });
   it('data-renders-correctly', () => {
+    // mocked the new Date response
+    const mockedDate = new Date('2018-02-28T09:39:59');
+    const originalDate = Date;
+
+    // overwritten the Date funtion
+    global.Date = jest.fn(() => mockedDate);
     const component = render(<TopConsumers {...TopConsumerStats[1]} />);
-    expect(component).toMatchSnapshot();
+    expect(component).toMatchSnapshot({});
+
+    // restored the original date function
+    global.Date.setDate = originalDate.setDate;
   });
   it('render-correctly-on-loading', () => {
     const component = render(<TopConsumers />);
     expect(component).toMatchSnapshot();
   });
 });
+/* eslint no-global-assign: 1 */
