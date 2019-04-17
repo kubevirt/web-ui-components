2de957914f9a3814c1b6468783749621a9a5bab5
yaacov <kobi.zamir@gmail.com>
2019-04-17 14:30:24 +0300

    fix subsystem health fixture

--- src/components/SubsystemHealth/fixtures/SubsystemHealth.fixture.js
+++ src/components/SubsystemHealth/fixtures/SubsystemHealth.fixture.js
@@ -1,24 +1,18 @@
 import { SubsystemHealth } from '../SubsystemHealth';
-import { OK_STATE } from '../../Dashboard/Health/HealthItem';
+import { OK_STATE, WARNING_STATE, ERROR_STATE, OCP_HEALTHY, CNV_ERROR, OCS_DEGRADED } from '../../Dashboard/Health/HealthItem';
 
 export const healthData = {
   k8sHealth: {
     state: OK_STATE,
-    response: 'ok',
+    message: OCP_HEALTHY,
   },
   cephHealth: {
-    state: OK_STATE,
-    result: [
-      {
-        value: [null, 0],
-      },
-    ],
+    message: OCS_DEGRADED,
+    state: WARNING_STATE,
   },
   kubevirtHealth: {
-    state: OK_STATE,
-    apiserver: {
-      connectivity: 'ok',
-    },
+    state: ERROR_STATE,
+    message: CNV_ERROR,
   },
 };
 
