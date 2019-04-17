02380387a0c45d6140ee9b05385be6e94f5176eb
yaacov <kobi.zamir@gmail.com>
2019-04-17 12:00:06 +0300

    update subsystem health fixture

--- src/components/SubsystemHealth/fixtures/SubsystemHealth.fixture.js
+++ src/components/SubsystemHealth/fixtures/SubsystemHealth.fixture.js
@@ -1,21 +1,21 @@
 import { SubsystemHealth } from '../SubsystemHealth';
 
 export const healthData = {
-  data: {
-    ocp: {
-      healthy: 0,
-      message: 'Error message',
-    },
-    cnv: {
-      healthy: 1,
-      message: 'Error message',
-    },
-    ceph: {
-      healthy: 2,
-      message: 'Error message',
+  k8sHealth: {
+    response: 'ok',
+  },
+  cephHealth: {
+    result: [
+      {
+        value: [null, 0],
+      },
+    ],
+  },
+  kubevirtHealth: {
+    apiserver: {
+      connectivity: 'ok',
     },
   },
-  loaded: true,
 };
 
 export default [
