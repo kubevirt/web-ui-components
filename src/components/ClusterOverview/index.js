4619ce383a1dfd66d480c39b28c82a93317c99de
Rastislav Wagner <rawagner@redhat.com>
2019-04-17 16:48:23 +0200

    Remove Overview/HealthBody and use Dashboard/HealthBody in Compliace

--- src/components/ClusterOverview/index.js
+++ src/components/ClusterOverview/index.js
@@ -4,9 +4,5 @@ export { ClusterOverviewContext } from './ClusterOverviewContext';
 // TODO move all these mocked data to component fixtures once they are not needed in web-ui
 // we do not include fixture files in build so we cannot reuse their mocked data
 export const complianceData = {
-  data: {
-    healthy: true,
-    message: 'All nodes compliant',
-  },
-  loaded: true,
+  result: 'ok',
 };
