994e809838a7332ba44dee7bde88a3da12351259
Rastislav Wagner <rawagner@redhat.com>
2019-04-18 10:15:19 +0200

    Use shallow during Top Consumers tests

--- src/components/StorageOverview/TopConsumers/tests/TopConsumers.test.js
+++ src/components/StorageOverview/TopConsumers/tests/TopConsumers.test.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { TopConsumers } from '../TopConsumers';
 import { TopConsumerStats } from '../fixtures/TopConsumers.fixture';
 
-/* eslint no-global-assign: 0 */
 describe('<TopConsumers />', () => {
   it('no-data-renders-correctly', () => {
-    const component = render(<TopConsumers {...TopConsumerStats[0]} />);
+    const component = shallow(<TopConsumers {...TopConsumerStats[0]} />);
     expect(component).toMatchSnapshot();
   });
   it('data-renders-correctly', () => {
-    // mocked the new Date response
-    const mockedDate = new Date('2018-02-28T09:39:59');
-    const originalDate = Date;
-
-    // overwritten the Date funtion
-    global.Date = jest.fn(() => mockedDate);
-    const component = render(<TopConsumers {...TopConsumerStats[1]} />);
+    const component = shallow(<TopConsumers {...TopConsumerStats[1]} />);
     expect(component).toMatchSnapshot({});
-
-    // restored the original date function
-    global.Date.setDate = originalDate.setDate;
   });
   it('render-correctly-on-loading', () => {
-    const component = render(<TopConsumers />);
+    const component = shallow(<TopConsumers />);
     expect(component).toMatchSnapshot();
   });
 });
-/* eslint no-global-assign: 1 */
