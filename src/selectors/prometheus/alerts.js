8160b4aa2e33566e7af1d130d65876db8db369c6
Ankush Behl <cloudbehl@gmail.com>
2019-04-24 23:21:38 +0530

    Show description of alert if available

--- src/selectors/prometheus/alerts.js
+++ src/selectors/prometheus/alerts.js
@@ -2,4 +2,5 @@ import { get } from 'lodash';
 
 export const getAlertSeverity = alert => get(alert, 'labels.severity');
 export const getAlertMessage = alert => get(alert, 'annotations.message');
+export const getAlertDescription = alert => get(alert, 'annotations.description');
 export const getStorageType = alert => get(alert, 'annotations.storage_type');
