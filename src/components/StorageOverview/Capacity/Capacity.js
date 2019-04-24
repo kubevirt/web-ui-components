4ef2edf350cfde92c2cb88b09ab71cb190957131
Matthew Carleton <mcarleton@redhat.com>
2019-04-24 09:53:38 -0300

    css updates

--- src/components/StorageOverview/Capacity/Capacity.js
+++ src/components/StorageOverview/Capacity/Capacity.js
@@ -15,8 +15,8 @@ import { formatBytes } from '../../../utils';
 import { getCapacityStats } from '../../../selectors';
 import { CapacityBody } from '../../Dashboard/Capacity/CapacityBody';
 
-export const Capacity = ({ capacityTotal, capacityUsed, LoadingComponent }) => (
-  <DashboardCard>
+export const Capacity = ({ capacityTotal, capacityUsed, LoadingComponent, className }) => (
+  <DashboardCard className="kubevirt-capacity__card">
     <DashboardCardHeader>
       <DashboardCardTitle>Capacity</DashboardCardTitle>
     </DashboardCardHeader>
@@ -40,14 +40,18 @@ Capacity.defaultProps = {
   capacityTotal: null,
   capacityUsed: null,
   LoadingComponent: InlineLoading,
+  className: null,
 };
 
 Capacity.propTypes = {
   capacityTotal: PropTypes.object,
   capacityUsed: PropTypes.object,
   LoadingComponent: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+  className: PropTypes.string,
 };
 
-export const CapacityConnected = () => (
-  <StorageOverviewContext.Consumer>{props => <Capacity {...props} />}</StorageOverviewContext.Consumer>
+export const CapacityConnected = ({ className }) => (
+  <StorageOverviewContext.Consumer>
+    {props => <Capacity {...props} className={className} />}
+  </StorageOverviewContext.Consumer>
 );
