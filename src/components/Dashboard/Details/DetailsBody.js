c64524c030bdc4e7a9f65009d3a3217e41f49c8a
matthew carleton <mcarleton@redhat.com>
2019-04-25 04:15:05 -0300

    css updates for details/charts (#383)

--- src/components/Dashboard/Details/DetailsBody.js
+++ src/components/Dashboard/Details/DetailsBody.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const DetailsBody = ({ children }) => <dl>{children}</dl>;
+export const DetailsBody = ({ children }) => <dl className="kubevirt-detail__list">{children}</dl>;
 
 DetailsBody.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
