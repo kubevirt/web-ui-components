f0bf49230ca1696564dc5f5c503bc066a4be2761
Marek Libra <mlibra@redhat.com>
2019-05-07 13:59:40 +0200

    Fix vddk mount path
    The source .tar already contains the "vmware-vix-disklib-distrib" directory.

--- src/k8s/requests/v2v/constants.js
+++ src/k8s/requests/v2v/constants.js
@@ -1,5 +1,5 @@
 export const CONVERSION_POD_TEMP_MOUNT_PATH = '/var/tmp';
-export const CONVERSION_POD_VDDK_MOUNT_PATH = '/opt/vmware-vix-disklib-distrib';
+export const CONVERSION_POD_VDDK_MOUNT_PATH = '/data/vddklib'; // will be /data/vddklib/vmware-vix-disklib-distrib
 
 export const CONVERSION_BASE_NAME = 'kubevirt-v2v-conversion';
 export const CONVERSION_GENERATE_NAME = `${CONVERSION_BASE_NAME}-`;
