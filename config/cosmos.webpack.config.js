62d3b20463b31cecf298abed7ed92757bb4ffd38
yaacov <kobi.zamir@gmail.com>
2019-05-23 14:24:40 +0300

    simplify cosmos config css-loader settings

--- config/cosmos.webpack.config.js
+++ config/cosmos.webpack.config.js
@@ -43,15 +43,8 @@ module.exports = {
       {
         test: /\.(jpg|jpeg|png|gif|svg|eot|otf|ttf|woff|woff2)$/,
         loader: 'file-loader',
-      },
-      {
+      },{
         test: /\.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
         use: ['style-loader', 'css-loader'],
       },
     ],
