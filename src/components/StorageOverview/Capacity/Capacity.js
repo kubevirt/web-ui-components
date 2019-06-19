6cd096792123a1f40eba0897fb3cd27124274067
AyushAmbastha <ambastha.ayush@gmail.com>
2019-06-19 11:15:24 +0530

    Capacity Dropdowns (#475)

--- src/components/StorageOverview/Capacity/Capacity.js
+++ src/components/StorageOverview/Capacity/Capacity.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col } from 'patternfly-react';
 
 import { InlineLoading } from '../../Loading';
 
@@ -9,30 +10,82 @@ import {
   DashboardCardHeader,
   DashboardCardTitle,
 } from '../../Dashboard/DashboardCard';
+import { Dropdown } from '../../Form/Dropdown';
+import { DashboardCardActionsBody } from '../../Dashboard/DashboardCard/DashboardCardActionsBody';
+
 import { StorageOverviewContext } from '../StorageOverviewContext';
 import { CapacityItem } from '../../Dashboard/Capacity/CapacityItem';
 import { formatBytes } from '../../../utils';
 import { getCapacityStats } from '../../../selectors';
 import { CapacityBody } from '../../Dashboard/Capacity/CapacityBody';
+import { TOTAL, CAPACITY, VMSVSPODS } from './strings';
+
+const capacityTypes = [TOTAL, CAPACITY, VMSVSPODS];
 
 export class Capacity extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      capacity: capacityTypes[0],
+    };
+  }
+
+  convertMetrics = (total, used, title) => ({ total, used, title });
+
+  changeCapacityMetric = newVal => {
+    this.setState({ capacity: newVal }, this.getNewCapacityMetric);
+  };
+
+  getNewCapacityMetric = () => {
+    switch (this.state.capacity) {
+      case TOTAL:
+        return this.convertMetrics(this.props.capacityTotal, this.props.capacityUsed, TOTAL);
+      case CAPACITY:
+        return this.convertMetrics(this.props.capacityRequested, this.props.capacityUsed, CAPACITY);
+      case VMSVSPODS:
+        return this.convertMetrics(this.props.podsCapacity, this.props.vmsCapacity, VMSVSPODS);
+      default:
+        return [];
+    }
+  };
+
   render() {
-    const { capacityTotal, capacityUsed, LoadingComponent } = this.props;
+    const { capacityTotal, capacityRequested, capacityUsed, vmsCapacity, podsCapacity, LoadingComponent } = this.props;
+
+    const capacitymetric = this.getNewCapacityMetric();
+    const isLoading = !capacityTotal && !capacityRequested && !capacityUsed && !vmsCapacity && !podsCapacity;
+
     return (
       <DashboardCard className="kubevirt-capacity__card">
         <DashboardCardHeader>
-          <DashboardCardTitle>Capacity</DashboardCardTitle>
+          <Row>
+            <Col lg={6} md={6} sm={6} xs={6}>
+              <DashboardCardTitle>Capacity</DashboardCardTitle>
+            </Col>
+            <Col>
+              <DashboardCardActionsBody>
+                <Dropdown
+                  id="capacity-type"
+                  value={this.state.capacity}
+                  onChange={this.changeCapacityMetric}
+                  choices={capacityTypes}
+                  disabled={isLoading}
+                  groupClassName="kubevirt-dropdown__group"
+                />
+              </DashboardCardActionsBody>
+            </Col>
+          </Row>
         </DashboardCardHeader>
         <DashboardCardBody>
           <CapacityBody>
             <CapacityItem
               id="capacity"
-              title="Total capacity"
-              used={getCapacityStats(capacityUsed)}
-              total={getCapacityStats(capacityTotal)}
+              title={capacitymetric.title}
+              used={getCapacityStats(capacitymetric.used)}
+              total={getCapacityStats(capacitymetric.total)}
               formatValue={formatBytes}
+              isLoading={!(capacitymetric.used && capacitymetric.total)}
               LoadingComponent={LoadingComponent}
-              isLoading={!(capacityUsed && capacityTotal)}
             />
           </CapacityBody>
         </DashboardCardBody>
@@ -43,13 +96,19 @@ export class Capacity extends React.PureComponent {
 
 Capacity.defaultProps = {
   capacityTotal: null,
+  capacityRequested: null,
   capacityUsed: null,
+  vmsCapacity: null,
+  podsCapacity: null,
   LoadingComponent: InlineLoading,
 };
 
 Capacity.propTypes = {
   capacityTotal: PropTypes.object,
+  capacityRequested: PropTypes.object,
   capacityUsed: PropTypes.object,
+  vmsCapacity: PropTypes.object,
+  podsCapacity: PropTypes.object,
   LoadingComponent: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
 };
 
@@ -59,6 +118,9 @@ export const CapacityConnected = () => (
       <Capacity
         capacityTotal={props.capacityTotal}
         capacityUsed={props.capacityUsed}
+        capacityRequested={props.capacityRequested}
+        vmsCapacity={props.vmsCapacity}
+        podsCapacity={props.podsCapacity}
         LoadingComponent={props.LoadingComponent}
       />
     )}
--- src/constants/storage.js
+++ src/constants/storage.js
@@ -1,8 +1,14 @@
 export const STORAGE_PROMETHEUS_QUERIES = {
   // from storage
   CEPH_STATUS_QUERY: 'ceph_health_status',
-  STORAGE_CEPH_CAPACITY_TOTAL_QUERY: 'ceph_cluster_total_bytes',
-  STORAGE_CEPH_CAPACITY_USED_QUERY: 'ceph_cluster_total_used_bytes',
+  STORAGE_CEPH_CAPACITY_TOTAL_QUERY: 'sum(max(kubelet_volume_stats_capacity_bytes) by (persistentvolumeclaim))',
+  STORAGE_CEPH_CAPACITY_USED_QUERY: 'sum(max(kubelet_volume_stats_used_bytes) by (persistentvolumeclaim))',
+  STORAGE_CEPH_CAPACITY_REQUESTED_QUERY:
+    'sum(max(kube_persistentvolumeclaim_resource_requests_storage_bytes) by (persistentvolumeclaim))',
+  STORAGE_CEPH_CAPACITY_VMS_QUERY:
+    '(sort(topk(5, sum(avg_over_time(kube_persistentvolumeclaim_resource_requests_storage_bytes[1h]) * on (namespace,persistentvolumeclaim) group_left(pod) kube_pod_spec_volumes_persistentvolumeclaims_info{pod=~"virt-launcher-.*"}) by (pod))))',
+  STORAGE_CEPH_CAPACITY_PODS_QUERY:
+    '(sort(topk(5, sum(avg_over_time(kube_persistentvolumeclaim_resource_requests_storage_bytes[1h]) * on (namespace,persistentvolumeclaim) group_left(pod) kube_pod_spec_volumes_persistentvolumeclaims_info) by (pod))))',
   CEPH_OSD_UP_QUERY: 'sum(ceph_osd_up)',
   CEPH_OSD_DOWN_QUERY: 'count(ceph_osd_up == 0.0) OR vector(0)',
 
