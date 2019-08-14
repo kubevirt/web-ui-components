496a3a519020e4985c1cefc32a097af136f01ea1
redallen <zallen@redhat.com>
2019-08-14 11:27:28 -0400

    fix ignoring files

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,7 +1,7 @@
 // https://babeljs.io/docs/en/next/options
+// ignore: [] is not respected here for some reason when used with @babel/cli
 
 module.exports = {
   presets: ['@babel/react'],
-  ignore: ['src/jest', 'src/cosmos', '**/tests/**', '**/fixtures/**'],
   plugins: ['@babel/plugin-proposal-class-properties'],
 };
